696cdc51a99c9906a136c8c0f21c2b33282198a6
Cedric Lapi <lapicedric@gmail.com>
2024-02-24 22:53:34 +0100

    dotenv and webtoken applied!

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,5 +1,7 @@
 
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const secret = process.env.SECRET_KEY;
 
 module.exports = {
     register: async (req, res) => {
@@ -9,6 +11,10 @@ module.exports = {
             res.status(400).json({message: "Email already exists"});
         }else{
             const newUser = await User.create(req.body);
+
+
+            const userToken = jwt.sign({_id:newUser.id, email:newUser.email}, secret, {expiresIn: "1d"})
+            console.log(userToken);
             res.json({message: "success", user: newUser});
         }
 
